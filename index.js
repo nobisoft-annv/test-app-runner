d7aae45ad9651bc4c41e1e1535a6e279262dc0bc
annv <annv@nobisoft.com.vn>
2024-01-01 23:00:56 +0700

    dev

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    const msg = 'Hello Node dev 1!\n'
-    console.log("hello dev")
-    res.end(msg);
+    if (req.url === '/500') {
+        res.statusCode = 500;
+        const errorMsg = 'Error ne (500)\n';
+        console.error(errorMsg);
+        res.end(errorMsg);
+    } else {
+        res.statusCode = 200;
+        const msg = 'Hello Node dev 1!\n';
+        console.log("hello dev");
+        res.end(msg);
+    }
 });
 
 server.listen(port, () => {
     console.log(`Server running on http://localhost:${port}/`);
+    console.log("hello dev");
 });
